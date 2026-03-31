6e9d9ef7f8314267a3bd6941ffdc38f4480a8e02
khokha1553 <khokha1553@gmail.com>
2026-03-31 07:52:18 +0200

    vercel \1

--- server.js
+++ server.js
@@ -11,5 +11,8 @@ app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/services", servicesRoutes);
+app.get("/", (req, res) => {
+  res.json({ message: "Daleel API is running ✅" });
+});
 
 export default app;
