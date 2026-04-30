090007ba72dda785e0b1af0ab55bdc2aa579018b
kholod osama <khokha1553@gmail.com>
2026-04-30 13:03:23 +0300

    Add chat routes to the server

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import categoriesRoutes from "./routes/categories.js";
 import usersRoutes from "./routes/users.js";
 import votesRouter from "./routes/votes.js";
 import settingsRoutes from "./routes/settings.js";
+import chatRoutes from "./routes/chat.js";
 dotenv.config();
 
 const app = express();
@@ -19,6 +20,7 @@ app.use("/categories", categoriesRoutes);
 app.use("/users", usersRoutes);
 app.use("/votes", votesRouter);
 app.use("/settings", settingsRoutes);
+app.use("/chat", chatRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Daleel API is running" });
