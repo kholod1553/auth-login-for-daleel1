8c293db66ae4fbee1adaa8e94dd370ad70c2e420
khokha1553 <khokha1553@gmail.com>
2026-05-02 07:23:55 +0300

    update gitignore

--- routes/chat.js
+++ routes/chat.js
@@ -16,8 +16,7 @@ router.post("/message", requireAuth, async (req, res) => {
 
     const response = await fetch(url, {
       method: "POST",
-      headers: { "Content-Type": "application/json" ,
-               "Authorization": `Bearer ${process.env.GEMINI_API_KEY}`},
+      headers: { "Content-Type": "application/json" , Authorization: `Bearer ${process.env.GEMINI_API_KEY}`},
       body: JSON.stringify({
         system_instruction: {
           parts: [{ text: SYSTEM_PROMPT }]
