30af797648f4bc34355e21645d4b89e80694a8a4
khokha1553 <khokha1553@gmail.com>
2026-05-02 10:02:45 +0300

    url

--- routes/chat.js
+++ routes/chat.js
@@ -12,7 +12,7 @@ router.post("/message", requireAuth, async (req, res) => {
   }
    console.log("GEMINI KEY:", process.env.GEMINI_API_KEY);
   try {
-    const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" + process.env.GEMINI_API_KEY;
+    const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=";
 
     const response = await fetch(url, {
       method: "POST",
